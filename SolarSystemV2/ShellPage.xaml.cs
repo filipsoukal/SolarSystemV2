f6e8c520c28e8fef9de62d7ee2f535a1afc42692
filipsoukal <soukal.filip.03@gmail.com>
2022-05-02 23:13:32 +0200

    Add project files.

---
+++ SolarSystemV2/ShellPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SolarSystemV2 {
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ShellPage : Shell {
+		public ShellPage() {
+			InitializeComponent();
+		}
+	}
+}
